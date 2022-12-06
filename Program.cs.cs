2a881e64a9b2569035b30afb5e1ef21177f2e35e
elmeddin mirzeyev <elmaddinshm@code.edu.az>
2022-12-07 01:12:19 +0400

    homework

---
+++ Program.cs.cs
@@ -0,0 +1,28 @@
+﻿
+int Qiymet = 95;
+
+if (Qiymet < 100 && Qiymet >= 91)
+{
+    Console.WriteLine("A");
+}
+else if (Qiymet < 91 && Qiymet >= 81)
+{
+    Console.WriteLine("B");
+}
+else if (Qiymet < 81 && Qiymet >= 71)
+{
+    Console.WriteLine("C");
+}
+else if (Qiymet < 71 && Qiymet >= 61)
+{
+    Console.WriteLine("D");
+}
+else if (Qiymet < 61 && Qiymet >= 51)
+{
+    Console.WriteLine("E");
+}
+else
+{
+    Console.WriteLine("Kesildiniz");
+}
+ 
